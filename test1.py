4da5b1ad6dc0219a93bfd2c240330c818b7e4d2e
Diego Rossi Corrêa <diegorossicorrea2000@hotmail.com>
2021-06-09 09:36:37 -0300

    Resolucoes da 1593, 1715, 2378, e 2444

--- test1.py
+++ test1.py
@@ -1,2 +1,2 @@
-prod = "abc"
-print(prod[len(prod)-2:])
+num = list(filter(lambda x: x == "1", str(bin(10))))
+print(num.count("1"))
---
+++ test2.py
@@ -0,0 +1,1 @@
+print(list(filter(lambda x: x == "1", str(bin(10)))))
