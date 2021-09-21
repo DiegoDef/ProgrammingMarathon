4771c70f4b9e435ca50be814ba491bb9689f1a11
Diego Rossi Corrêa <diegorossicorrea2000@gmail.com>
2021-09-21 20:52:15 -0300

    rename fase.py 2663.py

--- 2663.py
+++ 2663.py
@@ -1,13 +1,13 @@
 from collections import Counter
 
 pontuacoes = []
-quant_classificados = 0
 quant_competidores = int(input())
 vagas = int(input())
 for _ in range(quant_competidores):
     pontuacoes.append(int(input()))
 pontuacoes = Counter(pontuacoes)
 valores_ordenados = sorted(pontuacoes.items(), key=lambda ch: ch[0], reverse=True)
+quant_classificados = 0
 for chave, valor in valores_ordenados:
     quant_classificados += valor
     if quant_classificados >= vagas:
